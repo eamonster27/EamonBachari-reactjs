5dc53d51f5e863a7d090b6d220b58f209bee9acf
EamonBachari <eamonbachari@gmail.com>
2017-07-25 16:42:05 -0500

    Initial commit.

---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PilotName from './PilotName.js'
+
+export default class Form extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      pilot: this.props.initialName
+    }
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // FORM: HANDLE INPUT CHANGES
+  // handleNameChange below:
+  // See form lesson for details.
+  // Enter your code below:
+  handleNameChange(event){
+    this.setState({pilot: event.target.value});
+  }
+
+  //  FORM: SUBMIT METHOD
+  // handleSubmit below:
+  // See form lesson for details.
+  // Once the form is sumbited, two things need to happen: set the state of pilot to the input value.
+  // Then, set the value of the input back to an empty string.
+  // Enter your code below:
+  handleSubmit(event){
+    event.preventDefault();
+    this.setState({pilot: event.target.value});
+  }
+
+  render(){
+    return (
+      <div id="starwars-form-container">
+        <form id="starwars-form" onSubmit={this.handleSubmit}>
+          <label for="yourName">What is your name, pilot?</label>
+
+          <input onChange={this.handleNameChange} value={this.state.pilot} type="text" name="yourName" placeholder="Enter your name" />
+
+          <input type="submit" value="Submit" />
+          <PilotName name={this.state.pilot} />
+        </form>
+
+      </div>
+    )
+  }
+}
